9b0b8150704921271216681077e7500cb42d8b3d
Vishal sharma <106218302+Vishal20804@users.noreply.github.com>
2023-10-14 11:12:04 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,11 +5,6 @@ from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 application=Flask(__name__)
 
 app=application
-
-@app.route('/')
-def home_page():
-    return render_template('index.html')
-
 @app.route('/predict',methods=['GET','POST'])
 
 def predict_datapoint():
